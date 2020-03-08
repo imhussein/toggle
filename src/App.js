bffd8ec2cf88ef19cea9c482d1d333efd438ec47
muhammad <hussein@blockgemini.com>
2020-03-09 02:02:51 +0400

    adas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./main.scss";
 
-function App() {
+const Toggle = ({ ...rest }) => {
+  const [value, setValue] = useState({ toggle: false });
+
+  const onChange = e => {
+    setValue({ [e.target.name]: !value.toggle });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`toggle__wrapper ${
+        value.toggle ? "toggle__wrapper-checked" : ""
+      }`}
+    >
+      <div
+        className={`toggle__btn ${
+          value.toggle ? "toggle__checked toggle__btn-checked" : ""
+        }`}
+      >
+        <input
+          {...rest}
+          className="toggle__input"
+          checked={value.toggle}
+          onChange={onChange}
+        />
+        <label htmlFor={rest.id} className="toggle__label"></label>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => <Toggle name="toggle" id="toggle" type="checkbox" />;
 
 export default App;
